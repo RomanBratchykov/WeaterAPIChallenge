5721e89a857c6b2c92a24644c10def052ddf7ea3
RomanBratchykov <romanbratchykov@gmail.com>
2026-09-09 15:15:12 +0300

    [0] Initial commit

---
+++ src/main/kotlin/com/rb/weaterapichallenge/WeaterApiChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.rb.weaterapichallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeaterApiChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<WeaterApiChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/rb/weaterapichallenge/WeaterApiChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rb.weaterapichallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeaterApiChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
